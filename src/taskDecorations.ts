ee6c8fc94a36558a0f9c5e6504369932dfb57aae
Stefan Kernjak <stefan@thisotherthing.co>
2018-12-28 13:15:14 +0100

    project names are now highlighted

--- src/taskDecorations.ts
+++ src/taskDecorations.ts
@@ -1,24 +1,40 @@
 import * as vscode from 'vscode';
 
+const doneTaskDecorationType = vscode.window.createTextEditorDecorationType({
+  opacity: "0.2",
+  // borderWidth: '1px',
+  // borderStyle: 'solid',
+  // overviewRulerColor: 'blue',
+  // overviewRulerLane: vscode.OverviewRulerLane.Right,
+  light: {
+    // this color will be used in light color themes
+    // borderColor: 'darkblue'
+  },
+  dark: {
+    // this color will be used in dark color themes
+    // borderColor: 'lightblue'
+  }
+});
+
+const projectNameDecorationType = vscode.window.createTextEditorDecorationType({
+  fontWeight: "bold",
+  // borderWidth: '1px',
+  // borderStyle: 'solid',
+  // overviewRulerColor: 'blue',
+  // overviewRulerLane: vscode.OverviewRulerLane.Right,
+  light: {
+    // this color will be used in light color themes
+    // borderColor: 'darkblue'
+  },
+  dark: {
+    // this color will be used in dark color themes
+    // borderColor: 'lightblue'
+  }
+});
+
 export default function RunTaskDecorations(context: vscode.ExtensionContext) {
 
   let timeout : NodeJS.Timer | null = null;
-
-  const doneTaskDecorationType = vscode.window.createTextEditorDecorationType({
-    opacity: "0.2",
-    // borderWidth: '1px',
-    // borderStyle: 'solid',
-    // overviewRulerColor: 'blue',
-    // overviewRulerLane: vscode.OverviewRulerLane.Right,
-    light: {
-      // this color will be used in light color themes
-      // borderColor: 'darkblue'
-    },
-    dark: {
-      // this color will be used in dark color themes
-      // borderColor: 'lightblue'
-    }
-  });
   
   let activeEditor = vscode.window.activeTextEditor;
   if (activeEditor) {
@@ -49,11 +65,12 @@ export default function RunTaskDecorations(context: vscode.ExtensionContext) {
 		if (!activeEditor) {
 			return;
 		}
-		const doneTaskRegEx = /- .+@done/g;
 		const text = activeEditor.document.getText();
-		const doneTaskDecorations: vscode.DecorationOptions[] = [];
 
 		let match;
+
+		const doneTaskRegEx = /- .+@done/g;
+		const doneTaskDecorations: vscode.DecorationOptions[] = [];
 		while (match = doneTaskRegEx.exec(text)) {
 			const startPos = activeEditor.document.positionAt(match.index);
 			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
@@ -61,5 +78,15 @@ export default function RunTaskDecorations(context: vscode.ExtensionContext) {
 				doneTaskDecorations.push(decoration);
 		}
 		activeEditor.setDecorations(doneTaskDecorationType, doneTaskDecorations);
+
+		const projectRegEx = /^[ \t]*[a-zA-z0-9]+:$/gm;
+		const projectDecorations: vscode.DecorationOptions[] = [];
+		while (match = projectRegEx.exec(text)) {
+			const startPos = activeEditor.document.positionAt(match.index);
+			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+			const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'project' };
+			projectDecorations.push(decoration);
+		}
+		activeEditor.setDecorations(projectNameDecorationType, projectDecorations);
 	}
 }
