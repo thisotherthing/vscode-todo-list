24426bc0081088f873a5f3f1dc16ff656ea98173
Stefan Kernjak <stefan@thisotherthing.co>
2018-12-28 20:00:04 +0100

    toggling @done with alt+d works now

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const todoLanguageId = "todo";
+
+export const projectRegEx = /^[ \t]*[a-zA-z0-9]+:$/gm;
+export const doneTaskRegEx = /- .+@done/g;
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,15 @@
 import * as vscode from 'vscode';
 import RunTaskDecorations from './taskDecorations';
 
+import SubscribeToggleDone from "./commands/toggleDone";
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	RunTaskDecorations(context);
+
+	SubscribeToggleDone(context);
 }
 
 // this method is called when your extension is deactivated
